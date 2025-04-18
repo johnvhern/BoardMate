e448030fe13041dfda49408607c766a07384514c
johnvhern <191692002+johnvhern@users.noreply.github.com>
2025-04-18 09:32:13 +0800

    Add DPI awareness to prevent scaling issues
    This commit introduces a new using directive for
    System.Runtime.InteropServices and declares the
    SetProcessDPIAware method. This method is called
    in the Main function to ensure the application
    runs at the intended scale on high-DPI displays.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +9,15 @@ namespace BoardMate
 {
     internal static class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            SetProcessDPIAware(); // Prevents scaling
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Views.LoginPage());
